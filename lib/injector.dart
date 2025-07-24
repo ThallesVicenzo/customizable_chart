bc0a41bb6842bf9f8ef4b2a6cc091fb4c04100cf
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2025-07-24 15:42:25 -0300

    feat: add FallbackApiService and EnvironmentService to dependency injection

--- lib/injector.dart
+++ lib/injector.dart
@@ -2,6 +2,8 @@ import 'package:customizable_chart/l10n/global_app_localizations.dart';
 import 'package:customizable_chart/model/models/chart_data_model.dart';
 import 'package:customizable_chart/model/services/client/client_http.dart';
 import 'package:customizable_chart/model/services/client/dio/dio_client.dart';
+import 'package:customizable_chart/model/services/fallback_api_service.dart';
+import 'package:customizable_chart/model/services/environment.dart';
 import 'package:customizable_chart/model/repositories/llm_repository.dart';
 import 'package:customizable_chart/viewmodel/chart_viewmodel.dart';
 import 'package:customizable_chart/viewmodel/settings_viewmodel.dart';
@@ -16,9 +18,17 @@ Future<void> init() async {
 
   sl.registerSingleton<ClientHttp>(DioClient());
 
-  sl.registerSingleton<LlmRepository>(LLMRepositoryImplementation(sl()));
-
   sl.registerSingleton<GlobalAppLocalizations>(GlobalAppLocalizationsImpl());
+
+  sl.registerSingleton<FallbackApiService>(
+    FallbackApiServiceImplementation(sl()),
+  );
+
+  sl.registerSingleton<EnvironmentService>(
+    EnvironmentServiceImplementation(sl(), sl()),
+  );
+
+  sl.registerSingleton<LlmRepository>(LLMRepositoryImplementation(sl()));
 
   sl.registerFactory(() => ChartViewModel());
 
